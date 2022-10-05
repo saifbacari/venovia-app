cad5447da09b73cef6069a8f7ec40a6666dab6a2
Saif Bacari <saifbacari@gmail.com>
2022-10-05 21:51:47 +0200

    header banner structure ok

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,26 @@
+import { Fragment } from 'react';
+import classes from './Header.module.css'
+
+import headerImage from '../../assets/banner-main.png';
+
+const Header = props => {
+    return <Fragment>
+        <header className={classes.header}>
+            <h1>Venovia</h1>
+        <button>Pré-commander</button>    
+        </header>
+        <div className={classes['main-image']}>
+            <img src={headerImage} alt='immage banner'/>
+            <h1>La gestion de vos médicaments n’est plus un problème</h1>
+            <h2>Votre vie. Votre santé. Notre solution.</h2>
+            <ul>La fin de l’oubli de médicaments</ul>
+            <ul>La fin des mauvais dosages</ul>
+            <ul>La fin des problèmes de renouvellement d’ordonnance</ul>
+        </div>
+    </Fragment>
+}
+
+export default Header;
+
+//ajouter les css avec les classnames pour le header
+//le faire pour l'image banner-main(utiliser la 2e façon d'add du css comme on l'integre à Header.module.css)
