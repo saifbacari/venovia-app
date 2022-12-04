2e106cc312be0db95dce0d1bf0441995589c1619
Saifoullah Bacari <saifbacari@gmail.com>
2022-12-04 16:04:49 +0100

    banner finished

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -3,22 +3,30 @@ import MainCart from '../UI/MainCart';
 import classes from './Header.module.css'
 
 import headerImage from '../../assets/banner-main.png';
+import  logoBanner from '../../assets/logo-venovia.png';
 
 const Header = props => {
     return <Fragment>
         <MainCart className={classes.input}>
             <div className={classes['container-centered']}>
                 <navbar className={classes.navbar}>
-                        <h1>Venovia</h1>
+                        <img src={logoBanner} alt='img-banner' />
+                        <ul className={classes['list-navbar']}>
+                            <li>à propos</li>
+                            <li>E-box</li>
+                            <li>Contact</li>
+                        </ul>
                         <button>Pré-commander</button>    
                     </navbar>
                 <div className={classes['banner-image-text']}>
                         <div className={classes['banner-text']}>
                             <h1>La gestion de vos médicaments n’est plus un problème</h1>
-                            <h2>Votre vie. Votre santé. Notre solution.</h2>
-                            <ul>La fin de l’oubli de médicaments</ul>
-                            <ul>La fin des mauvais dosages</ul>
-                            <ul>La fin des problèmes de renouvellement d’ordonnance</ul>
+                            <h3>Votre vie. Votre santé. Notre solution.</h3>
+                            <ul className={classes['list-banner']}>
+                                <li>La fin de l’oubli de médicaments</li>
+                                <li>La fin des mauvais dosages</li>
+                                <li>La fin des problèmes de renouvellement d’ordonnance</li>
+                            </ul>
                         </div>
                         <img src={headerImage} alt='immage banner'/>      
                 </div>           
